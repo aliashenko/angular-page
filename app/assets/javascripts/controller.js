553f98ed7fc6d53f9badd47be9b7a4e127d164eb
Anna Romanova <lyashenko.ann@gmail.com>
2014-11-04 17:21:05 +0200

    Fix headers for authorization

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -1,20 +1,7 @@
-// myApp.controller('AngularRequestCtrl', ['$scope',
-//   function($scope) {
-//    $scope.phones = [
-//      {'name': 'Nexus S',
-//       'snippet': 'Fast just got faster with Nexus S.'},
-//      {'name': 'Motorola XOOM™ with Wi-Fi',
-//       'snippet': 'The Next, Next Generation tablet.'},
-//      {'name': 'MOTOROLA XOOM™',
-//       'snippet': 'The Next, Next Generation tablet.'}
-//    ];
-//   }]);
-
-// var app = angular.module('myApp', []);
-
 myApp.config(function($httpProvider) {
   //Enable cross domain calls
   $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.headers.common['Authorization'] = 'Token token=0ec3bd53cddc30075330c6c682f74784';
 
   //Remove the header used to identify ajax call  that would prevent CORS from working
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -23,9 +10,8 @@ myApp.config(function($httpProvider) {
 myApp.controller('AngularRequestCtrl', function($scope, $http) {
 
   $scope.get = function() {
-    $http.defaults.headers.common['auth-token'] = 'C3PO R2D2';
 
-    $http.get("http://inyourcornerbackend.25u.com/api/v1/clients/me ").success(function(result) {
+    $http.get("http://localhost:3001/api/v1/clients/me").success(function(result) {
         console.log("Success", result);
         $scope.result = result;
     }).error(function() {
